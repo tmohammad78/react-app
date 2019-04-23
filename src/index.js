60d0d012e022fe01307fbfc5b0af85ef2cb3938a
tmohammad78 <mohammadtaheri506@gmail.com>
2019-04-23 17:35:13 +0430

    starting react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'; //JSX
+import ReactDOM from 'react-dom'; //Render a React element into the DOM
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Test=()=>{
+    // return 'mohammad'; // its for return just one tag and things or one section
+    //  React.createElement('h1',{className:'title'},React.createElement('div',{className:'box-1'}));
+    return ( //use ( ) for using alot element because return see just one element and use ClassName in react instead of class 
+        <div className="newElement"> 
+            <h1>hello react</h1>
+            <div>Hey</div>
+        </div>
+    )
+}
+ReactDOM.render(<Test/>,document.querySelector('#APP'));  //render is method of reactDom  ,enter component
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+file2.js
+
+export const name="mohammad";
+export const pass="123"
+export defualt "ok"
+
+
+file1.js
+
+import {name as username} from "file2";
+
+import * as data from "file2"
+
+data.namedata.pass
+
