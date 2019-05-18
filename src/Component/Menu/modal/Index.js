1857978da059ac17789698bd016f5a449ea28fa4
tmohammad78 <mohammadtaheri506@gmail.com>
2019-05-18 09:49:10 +0430

    changing

--- src/Component/Menu/modal/Index.js
+++ src/Component/Menu/modal/Index.js
@@ -12,21 +12,7 @@ class Modal extends Component {
         </header>
         <section>
           <figure className="figure" />
-          <div className="details">
-            <h2> {this.props.title} </h2>
-            <div className="ingredient">{this.props.ingredient}</div>
-            <footer>
-              <span className="price">{this.props.price} </span>
-              <div className="quantity-holder">
-                <button className="btn-plus" />
-                <span classNameName="quantity"> </span>
-                <button classNameName="btn-minus" />
-              </div>
-            </footer>
-          </div>
-          <div className="parent-btn">
-            <button className="btn-cart">افزودن به سبد خرید</button>
-          </div>
+          
         </section>
       </div>
     );
