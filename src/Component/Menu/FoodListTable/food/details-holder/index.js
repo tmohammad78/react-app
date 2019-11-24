dca586b782cdc1847121025d91825ac9ff06483f
mohammad <mohammadtaheri506@gmail.com>
2019-11-25 00:05:47 +0330

    add loader for svg and add like food feature

--- src/Component/Menu/FoodListTable/food/details-holder/index.js
+++ src/Component/Menu/FoodListTable/food/details-holder/index.js
@@ -1,21 +1,29 @@
 import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 
 import Ingredient from '../foodDetails/ingredient';
 import Price from '../foodDetails/price';
 import QtyHolder from '../foodDetails/qty-holder';
 import Tittle from '../foodDetails/Tittle';
 import LikeFood from '../foodDetails/likeFood';
+import { addFoodLike } from 'services/likeFood/action';
 import './style.scss';
 
 const Details = ({ food }) => {
+  const dispatch = useDispatch();
+
+  const handleLikeFood = () => {
+    dispatch(addFoodLike(food));
+  };
+
   const Detail = () => {
     return (
       <React.Fragment>
         <Tittle tittle={food.title} />
         <Ingredient ingredient={food.ingredient} />
         <Price price={food.price} />
-        <LikeFood />
+        <LikeFood handleLikeFood={handleLikeFood} />
       </React.Fragment>
     );
   };
---
+++ src/services/likeFood/actionType.js
@@ -0,0 +1,1 @@
+export const ADD_TO_LIKE = 'ADD_TO_LIKE';
