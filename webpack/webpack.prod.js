3fa2bac10588772d26f3fc735d276554729028f7
mohammad <mohammadtaheri506@gmail.com>
2020-02-18 19:15:12 +0330

    check

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,15 +1,18 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const autoprefixer = require("autoprefixer");
-const commonVariables = require("./commonVariables");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
+const commonVariables = require('./commonVariables');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = Object.keys(commonVariables.languages).map(function(language) {
   return {
-    mode: "production",
+    mode: 'production',
     entry: {
-      app: ['core-js/stable',
-      'regenerator-runtime/runtime', `${commonVariables.appEntry}/index.js`]
+      app: ['core-js/stable', 'regenerator-runtime/runtime', `${commonVariables.appEntry}/index.js`]
     },
     output: {
       chunkFilename: `[name].[chunkhash:8].js`
@@ -23,16 +26,16 @@ module.exports = Object.keys(commonVariables.languages).map(function(language) {
               loader: MiniCssExtractPlugin.loader
             },
             {
-              loader: "css-loader",
+              loader: 'css-loader',
               options: {
-                minimize: true,
+                // minimize: true,
                 sourceMap: false
               }
             },
             {
-              loader: "postcss-loader",
+              loader: 'postcss-loader',
               options: {
-                ident: "postcss",
+                ident: 'postcss',
                 sourceMap: false,
                 plugins: () => [
                   autoprefixer({
@@ -47,29 +50,54 @@ module.exports = Object.keys(commonVariables.languages).map(function(language) {
               }
             },
             {
-              loader: "sass-loader",
+              loader: 'sass-loader',
               options: {
-                data: `$pageDirection: "${
-                  commonVariables.languages[language]["langDirection"]
-                }";`,
+                data: `$pageDirection: "${commonVariables.languages[language]['langDirection']}";`,
                 sourceMap: false
+              }
+            }
+          ]
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: 'babel-loader'
+            },
+            {
+              loader: 'react-svg-loader',
+              options: {
+                jsx: true // true outputs JSX tags
               }
             }
           ]
         }
       ]
     },
+    optimization: {
+      concatenateModules: true,
+      minimizer: [new UglifyJsPlugin()]
+    },
+
     plugins: [
+      new BundleAnalyzerPlugin(),
       new FriendlyErrorsWebpackPlugin(),
+      new CompressionPlugin(),
+      new MinifyPlugin(),
       new MiniCssExtractPlugin({
-        filename: "[name]." + language + ".css",
-        chunkFilename: "[id]." + language + ".css"
-        // filename: `static/css/[name].[contenthash].css`,
-        // chunkFilename: `static/css/[id].[contenthash].css`,
-	  }),
-	  new HtmlWebpackPlugin({
-		minify:true // minify html files
-	})
+        filename: '[name].' + language + '.css',
+        chunkFilename: '[id].' + language + '.css',
+        filename: `static/css/[name].[contenthash].css`,
+        chunkFilename: `static/css/[id].[contenthash].css`
+      }),
+      //   webpack.optimize.DedupePlugin
+
+      new HtmlWebpackPlugin({
+        // minify: true, // minify html files
+        minify: {
+          collapseWhitespace: true
+        }
+      })
     ]
   };
 });
