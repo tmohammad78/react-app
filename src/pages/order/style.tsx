501a876d9ce1af7a4b699b29007528825d20d453
mohammad <mohammadtaheri506@gmail.com>
2020-04-16 11:53:54 +0430

    fix bug in style subFood

--- src/pages/order/style.tsx
+++ src/pages/order/style.tsx
@@ -51,8 +51,8 @@ export const RestMenuHolder = styled(Clear).attrs({
 			display: none;
     }
     .wrapper {
-			width: 71%;
-      float: right;
+		width: 71%;
+      	float: right;
     }
   }
 `;
